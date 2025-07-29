b3b2a78f98d0c9b66bcc9bac62704c659edcf559
Adityaaafr <adityaforreal05@gmail.com>
2025-07-29 19:02:36 +0530

    deleted the unecessary files that were present in the repository!

--- Main_files/create_db.py
+++
@@ -1,7 +0,0 @@
-from db.database import Base, engine
-from db.models import Employee
-
-# This will create the tables in the database
-Base.metadata.create_all(bind=engine)
-
-print("✅ Tables created successfully.")
